2a868369d5f46e540476510e386695dc7f7e862c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 09:14:00 -0400

    complete matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,36 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([])
+  }
+  let counter = 1, startRow = 0, startColumn = 0, endRow = n - 1, endColumn = n - 1;
+
+  while (startRow <= endRow || startColumn <= endColumn) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++
+    }
+    endRow--;
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++
+    }
+    startColumn++
+  }
+  return results
+}
 
 module.exports = matrix;
