4fa83f295d9c58630b432b8f51a1b3d39e4d0bd2
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 09:13:49 -0400

    complete maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let dict = {}
+  let solution = ''
+  let maxValue = 0
+
+  for (let chr of str) {
+    if (dict[chr]) {
+      dict[chr]++
+      } else {
+      dict[chr] = 1
+    }
+  }
+
+  for (let chr in dict) {
+    dict[chr] > maxValue ? (maxValue = dict[chr], solution = chr) : null
+  }
+
+  return solution
+}
 
 module.exports = maxChar;
