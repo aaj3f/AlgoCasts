394e458acf60f780699a15b5973c5cdde96fe9fb
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 09:14:29 -0400

    complete level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const array = [root, 'token']
+  const counters = [0]
+  while (array.length > 1) {
+    const node = array.shift();
+
+    if (node === 'token') {
+      counters.push(0);
+      array.push('token');
+    } else {
+      array.push(...node.children);
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+}
 
 module.exports = levelWidth;
