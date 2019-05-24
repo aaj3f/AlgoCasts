83729552cb48e33cd8b75ed636eda816a69511f9
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 09:10:56 -0400

    complete validate

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let array = str.match(/[aeiou]/ig)
+  return (array ? array.length : 0)
+}
 
 module.exports = vowels;
