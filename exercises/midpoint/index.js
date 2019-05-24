0300c4f3baff5ccbe9ca5526d2725625a57fc243
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-24 09:13:39 -0400

    complete midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,13 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+  let [slow, fast] = [list.getFirst(), list.getFirst()];
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next
+    slow = slow.next
+  }
+  return slow
+}
 
 module.exports = midpoint;
